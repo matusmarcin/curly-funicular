eff08a79d479ccdd3c52e165f2d58201f0a59d77
Matus Marcin <matus.marcin@globallogic.com>
2016-09-08 23:51:27 +0200

    Added payments table

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import NumberFormat from 'react-number-format';
 
 class App extends React.Component {
 	constructor() {
@@ -25,14 +26,18 @@ class App extends React.Component {
 		let remaining = this.state.debt;
 		let interest =  0;
 		this.periods = 0;
-		this.accumulatedInterest = 0
+		this.accumulatedInterest = 0;
+		this.schema = [];
 		while(this.state.debt && this.state.apr && this.state.payment && remaining > 0 && this.periods < 300) {
 			interest = remaining*(this.state.apr/100)/12;
 			this.accumulatedInterest += interest;
 			remaining -= this.state.payment-interest;
+			if(remaining < 0) remaining = 0;
 			this.periods++;
-			// console.log(this.periods, remaining, interest, this.state.payment);
+			this.schema.push({"period": this.periods, "remaining": remaining, "accumulatedInterest": this.accumulatedInterest});
+			console.log(this.periods, remaining, interest, this.accumulatedInterest, this.state.payment);
 		}
+		// console.log(this.schema);
 		this.forceUpdate();
 	}
 	render() {
@@ -47,6 +52,7 @@ class App extends React.Component {
 				<hr />
 				<p>You owe: {this.state.debt}<br />Interest: {this.state.apr}%<br />Payment: {this.state.payment}</p>
 				<p>You will pay it off in <strong>{this.periods} months</strong> with accumulated interest <strong>{this.accumulatedInterest.toFixed(2)} EUR</strong></p>
+				<PaymentsTable ref="payments" schema={this.schema} update={this.update} />
 			</div>
 		);
 	}
@@ -57,6 +63,39 @@ class Input extends React.Component {
 		return (
 			<input type="text"
 				onChange={this.props.update} />
+		);
+	}
+}
+
+class PaymentsTable extends React.Component {
+	render() {
+		var rows = [];
+		this.props.schema.forEach(function(row) {
+			rows.push(<PaymentsTableRow row={row} />);
+		});
+		return (
+			<table style={{border: '1px solid #444'}}>
+				<thead>
+					<tr>
+						<th>Months</th>
+						<th>Remaining</th>
+						<th>Accumulated Interest</th>
+					</tr>
+				</thead>
+				<tbody>{rows}</tbody>
+			</table>
+		);
+	}
+}
+
+class PaymentsTableRow extends React.Component {
+	render() {
+		return (
+			<tr>
+				<td>{this.props.row.period}.</td>
+				<td>{this.props.row.remaining.toFixed(0) + ' €'}</td>
+				<td>{this.props.row.accumulatedInterest.toFixed(0) + ' €'}</td>
+			</tr>
 		);
 	}
 }
