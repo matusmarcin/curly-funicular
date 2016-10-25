6f2e7485354b8820e00e4573858ea3df29fb2b2c
Matus Marcin <matus.marcin@globallogic.com>
2016-10-26 00:31:01 +0200

    Mocha + jsdom agony

--- webpack.config.js
+++ webpack.config.js
@@ -13,10 +13,7 @@ module.exports = {
 			{
 				test: /\.js$/,
 				exclude: /node_modules/,
-				loader: 'babel',
-				query: {
-					presets: ['es2015', 'react']
-				}
+				loader: 'babel-loader'
 			}
 		]
 	}
